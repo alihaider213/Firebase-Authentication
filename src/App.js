e8c997aacb2a2efe2a61290023145eea29df152c
alihader213 <alihader213@gmail.com>
2022-09-21 10:44:40 -0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import './App.scss';
+import AddProduct from './pages/AddProduct';
+import ReadProducts from './pages/ReadProducts';
+//import AddUser from './pages/AddUser';
+//import Login from './pages/Login';
+import Register from './pages/Register';
+import "bootstrap/dist/js/bootstrap.bundle"
+import AddUser from './pages/AddUser';
+
+function App() {
+
+  return (
+
+    <>
+      {/* <Register /> */}
+      {/* <ReadProducts/> */}
+      {/* <AddProduct/> */}
+      <AddUser/>
+    
+    </>
+  );
+}
+
+export default App;
